d0003fa4335fec5d2cce189f1f057f7c04357325
jimothy911 <150150156+jimothy911@users.noreply.github.com>
2023-12-13 09:27:48 -0500

    I added all the models for the monsters and the main character. I also figured out what type of game to do.  A three wave survival game.

--- FinalBoss.java
+++ FinalBoss.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Enemy here.
+ * Write a description of class FinalBoss here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Enemy extends Actor
+public class FinalBoss extends Actor
 {
     /**
-     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * Act - do whatever the FinalBoss wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
+    public void act() 
     {
         // Add your action code here.
-    }
+    }    
 }
